7ac699559dc6c983eddc710a802ea9814fac0e00
kasun <https://github.com/GDSE-59-60-Project-Zone/Validations.git>
2022-10-11 15:14:05 +0530

    content update

--- assignments/JS/SPA/controllers/OrderController.js
+++ assignments/JS/SPA/controllers/OrderController.js
@@ -1,5 +1,6 @@
 //Load Customers Ids Into ComboBox
 function loadAllCustomersForOption() {
+    $("#inputCustomerID").empty();
     for (let customerElement of customers) {
         $("#inputCustomerID").append(`<option>${customerElement.id}</option>`);
     }
@@ -8,6 +9,7 @@ function loadAllCustomersForOption() {
 
 //Load Items Code Into ComboBox
 function loadAllItemsForOption(){
+    $('#inputItemID').empty();
     for(let itemElement of items){
         $('#inputItemID').append(`<option>${itemElement.code}</option>`);
     }
