5abfdd1538d44a0b5b40cc1866ffc5d29735e0ce
kasun <https://github.com/GDSE-59-60-Project-Zone/Validations.git>
2022-10-11 20:14:13 +0530

    added order detail section

--- assignments/JS/SPA/controllers/PageLoadingController.js
+++ assignments/JS/SPA/controllers/PageLoadingController.js
@@ -7,6 +7,8 @@ $(document).ready(function () {
     $('#customerSection').hide();
     $('#itemSection').hide();
     $('#orderSection').hide();
+    $('#orderDetailsSection').hide();
+
 });
 
 //Start When home click other section hide
@@ -15,6 +17,7 @@ $('#home').click(function () {
     $('#customerSection').css('display', 'none');
     $('#itemSection').css('display', 'none');
     $('#orderSection').css('display', 'none');
+    $('#orderDetailsSection').css('display', 'none');
 });
 // End When home click other section hide
 
@@ -24,6 +27,7 @@ $('#customer').click(function () {
     $('#customerSection').css('display', 'block');
     $('#itemSection').css('display', 'none');
     $('#orderSection').css('display', 'none');
+    $('#orderDetailsSection').css('display', 'none');
 });
 // End When customer click other section hide
 
@@ -33,6 +37,7 @@ $('#item').click(function () {
     $('#customerSection').css('display', 'none');
     $('#itemSection').css('display', 'block');
     $('#orderSection').css('display', 'none');
+    $('#orderDetailsSection').css('display', 'none');
 });
 // End When item click other section hide
 
@@ -42,9 +47,20 @@ $('#order').click(function () {
     $('#customerSection').css('display', 'none');
     $('#itemSection').css('display', 'none');
     $('#orderSection').show();
+    $('#orderDetailsSection').hide();
 
     $("#txtOrderID").val(generateOrderID());
 });
+// End When order click other section hide
 
+// Start When order-details click other section hide
+$('#orderDetails').click(function () {
+    $('#dashboard').css('display', 'none')
+    $('#customerSection').css('display', 'none');
+    $('#itemSection').css('display', 'none');
+    $('#orderSection').hide();
+    $('#orderDetailsSection').css('display', 'block');
 
+    $("#txtOrderID").val(generateOrderID());
+});
 // End When order click other section hide
