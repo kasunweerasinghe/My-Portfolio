a3c22de407b921568f55eb9a4edd02830306bf40
Kasun-Weerasinghe <kasunrachintha2018@gmail.com>
2022-11-22 12:38:08 +0530

    initialize function for Move Snake

--- assignments/JS/Game/assets/js/script.js
+++ assignments/JS/Game/assets/js/script.js
@@ -4,24 +4,54 @@ $(function () {
     //select context of canvas
     var ctx = canvas.getContext('2d');
 
+    //snake array
     var snake = [
-        {x: 50, y: 100,},
-        {x: 50, y: 90,},
-        {x: 50, y: 80,}
+        {x: 50, y: 100,oldX:0, oldY:0},
+        {x: 50, y: 90,oldX:0, oldY:0},
+        {x: 50, y: 80,oldX:0, oldY:0}
 
     ];
 
     var snakeWidth = snakeHeight = 10;
+    var blockSize = 10;
+
+    //create a key values for variables
+    const LEFT = 37;
+    const UP = 38;
+    const RIGHT = 40;
+    const DOWN = 39;
+
+    var keyPressed = DOWN;
 
     setInterval(gameLoop, 1000);
 
     //create function for loop the game
-    function gameLoop(){
-        console.log("loop run")
+    function gameLoop() {
+        console.log("loop run");
+        clearCanvas();
+        moveSnake();
         drawSnake();
     }
 
 
+    //function for Move Snake
+    function moveSnake() {
+        $.each(snake, function (index, value) {
+            snake[index].oldX = value.x;
+            snake[index].oldY = value.y;
+            if (index == 0) {
+                if (keyPressed == DOWN) {
+                    snake[index].y = value.y + blockSize;
+                }
+            } else {
+                snake[index].x = snake[index -1].oldX;
+                snake[index].y = snake[index -1].oldY;
+            }
+        });
+    }
+
+
+    //draw snake function
     function drawSnake() {
         $.each(snake, function (index, value) {
             ctx.fillStyle = 'red';
@@ -31,6 +61,11 @@ $(function () {
             ctx.strokeStyle = 'white';
             ctx.strokeRect(value.x, value.y, snakeWidth, snakeHeight);
         });
+    }
+
+    //clear canvas
+    function clearCanvas() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
     }
 
 });
